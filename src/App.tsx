38b7e05a16bde8b6a755a44b967a2bb59f76af72
t.onodera <t.onodera.1.2.1.5@gmail.com>
2022-10-23 00:02:00 +0900

    動作は完成

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,43 @@
-import { useEffect, useState } from 'react';
+import { useState } from 'react';
 import { invoke } from '@tauri-apps/api/tauri';
 import './App.css';
 
 function App() {
-  const [isBusy, setIsBusy] = useState(true);
+  const durationTime = 60 * 1000; // 1分に一回押下する
+  const [isBusy, setIsBusy] = useState(false);
+  const [timerId, setTimerId] = useState(null as unknown as NodeJS.Timer);
+
+  // tauriコマンド実行
   const pressControlKey = async () => {
     await invoke('press_control_key');
-    setIsBusy(true);
   };
 
-  const releaseControlKey = async () => {
-    await invoke('release_control_key');
-    setIsBusy(false);
+  // コントロールキー押下プロセス開始
+  const startPressControlKeyProcess = () => {
+    setIsBusy(true);
+    const id = setInterval(() => {
+      pressControlKey();
+    }, durationTime);
+    setTimerId(id);
+  };
+
+  // コントロールキー押下プロセス停止
+  const stopPressControlKeyProcess = () => {
+    if (isBusy) {
+      setIsBusy(false);
+      clearInterval(timerId);
+    }
   };
 
   return (
     <>
       <div>
-        <button type="button">起動する</button>
-        <button type="button">停止する</button>
+        <button type="button" onClick={startPressControlKeyProcess}>
+          起動する
+        </button>
+        <button type="button" onClick={stopPressControlKeyProcess}>
+          停止する
+        </button>
       </div>
     </>
   );
